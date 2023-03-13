b9d71f3806c255aa1022c9befba5054ab1bdc909
josephiskingkong <iloveuyungcole@gmail.com>
2023-03-13 15:06:09 +0700

    Change directory's var name

--- fourth_lab/TextFileSearcher.cs
+++ fourth_lab/TextFileSearcher.cs
@@ -6,9 +6,9 @@ namespace fourth_lab;
 
 public class TextFileSearcher
 {
-    public IEnumerable<TextFile> SearchFiles(string directoryPath, string[] keywords)
+    public IEnumerable<TextFile> SearchFiles(string DirectoryPath, string[] keywords)
     {
-        var files = Directory.GetFiles(directoryPath, "*.txt", SearchOption.AllDirectories);
+        var files = Directory.GetFiles(DirectoryPath, "*.txt", SearchOption.AllDirectories);
         return files.Select(file => new TextFile(file, File.ReadAllText(file)))
         .Where(textFile => keywords.Any(keyword => textFile.Content.Contains(keyword, StringComparison.OrdinalIgnoreCase)));
     }
